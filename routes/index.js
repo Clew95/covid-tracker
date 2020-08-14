373a060669906107698ebaed11ac84bde3cc7082
Marco Quintano <34493566+Clew95@users.noreply.github.com>
2020-08-14 21:58:57 +0200

    Update project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('landing', { title: 'Covid Tracker', subtitle: 'Check if the risk is high or not' });
 });
 
 module.exports = router;
