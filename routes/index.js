1436bc12bd6845e18bcca3dcb549906a7a26fd23
Marco Quintano <34493566+Clew95@users.noreply.github.com>
2020-08-17 02:35:58 +0200

    Update /covid page with sidebar and navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('landing', { title: 'Covid Tracker', subtitle: 'Check if the risk is high or not' });
+	res.render('landing');
 });
 
 module.exports = router;
