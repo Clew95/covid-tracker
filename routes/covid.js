44c39ede83fc797ed58cdfa23a6fd87f146af8ed
Clew95 <marco.quintano1995@gmail.com>
2020-08-19 03:35:29 +0200

    Update the stats-card element

--- routes/covid.js
+++ routes/covid.js
@@ -2,7 +2,7 @@ const express = require('express'),
 	router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render('covid');
+	res.render('covid', { baseUrl: req.baseUrl });
 });
 
 module.exports = router;
