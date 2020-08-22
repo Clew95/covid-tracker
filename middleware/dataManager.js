1a1437dddf75494fd44cd1d51bcbd2f283d090b8
Clew95 <marco.quintano1995@gmail.com>
2020-08-22 17:21:17 +0200

    Update to connect website to covid italian data with axios request

---
+++ middleware/dataManager.js
@@ -0,0 +1,14 @@
+const { data } = require('autoprefixer');
+
+let dataManager = {};
+
+//This is a funciton to covert
+dataManager.numberWithCommas = function(x) {
+	return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, '.');
+};
+
+dataManager.isANumber = function(x) {
+	return !isNaN(x);
+};
+
+module.exports = dataManager;
--- routes/covid.js
+++ routes/covid.js
@@ -1,8 +1,27 @@
+const { response } = require('express');
+
 const express = require('express'),
+	axios = require('axios').default,
 	router = express.Router();
 
+const dataManager = require('../middleware/dataManager');
+
 router.get('/', (req, res) => {
-	res.render('covid', { baseUrl: req.baseUrl });
+	axios
+		.get(
+			'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json'
+		)
+		.then((response) => {
+			const DATA_BUNCH = response.data[0];
+
+			for (let key in DATA_BUNCH) {
+				if (dataManager.isANumber(DATA_BUNCH[key])) {
+					DATA_BUNCH[key] = dataManager.numberWithCommas(DATA_BUNCH[key]);
+				}
+			}
+
+			res.render('covid', { baseUrl: req.baseUrl, data: DATA_BUNCH });
+		});
 });
 
 module.exports = router;
