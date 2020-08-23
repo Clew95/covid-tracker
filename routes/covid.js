16cb4ae4dc050229b92ffbafec1f94cda38c4e0d
Clew95 <marco.quintano1995@gmail.com>
2020-08-23 23:52:00 +0200

    Axios request completed for Overview and added incremento and decremento arrows to show if numbers are better or not

--- routes/covid.js
+++ routes/covid.js
@@ -1,5 +1,3 @@
-const { response } = require('express');
-
 const express = require('express'),
 	axios = require('axios').default,
 	router = express.Router();
@@ -9,18 +7,28 @@ const dataManager = require('../middleware/dataManager');
 router.get('/', (req, res) => {
 	axios
 		.get(
-			'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json'
+			'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json'
 		)
 		.then((response) => {
-			const DATA_BUNCH = response.data[0];
+			const DATA_BUNCH = response.data;
+			const TODAY = DATA_BUNCH.length - 1;
+			const DATA_SETTINGS = {
+				incremento_nuovi_positivi       :
+					parseInt(DATA_BUNCH[TODAY]['nuovi_positivi']) - parseInt(DATA_BUNCH[TODAY - 1]['nuovi_positivi']) >
+					0
+						? true
+						: false,
+				incremento_attualmente_positivi :
+					parseInt(DATA_BUNCH[TODAY]['variazione_totale_positivi']) > 0 ? true : false
+			};
 
-			for (let key in DATA_BUNCH) {
-				if (dataManager.isANumber(DATA_BUNCH[key])) {
-					DATA_BUNCH[key] = dataManager.numberWithCommas(DATA_BUNCH[key]);
+			for (let key in DATA_BUNCH[TODAY]) {
+				if (DATA_BUNCH[TODAY][key] && dataManager.isANumber(DATA_BUNCH[TODAY][key])) {
+					DATA_BUNCH[TODAY][key] = dataManager.numberWithCommas(DATA_BUNCH[TODAY][key]);
 				}
 			}
 
-			res.render('covid', { baseUrl: req.baseUrl, data: DATA_BUNCH });
+			res.render('covid', { baseUrl: req.baseUrl, data: DATA_BUNCH, settings: DATA_SETTINGS });
 		});
 });
 
