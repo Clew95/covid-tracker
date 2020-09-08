6e90fabee80f07789267f9a9095a1c3709ac2e4d
Clew95 <marco.quintano1995@gmail.com>
2020-09-08 16:20:31 +0200

    Update Regioni's data

--- routes/regioni.js
+++ routes/regioni.js
@@ -11,4 +11,17 @@ router.get('/', (req, res) => {
 		});
 });
 
+router.get('/:regione', (req, res) => {
+	res.render('working', { baseUrl: req.baseUrl });
+
+	/*	axios
+		.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json')
+		.then((response) => {
+			const DATA_BUNCH = response.data.filter(function(e) {
+				return e.denominazione_regione.toLowerCase() === req.params.regione;
+			});
+
+			res.render('regionDetails', { baseUrl: req.baseUrl, data: DATA_BUNCH });
+		});*/
+});
 module.exports = router;
