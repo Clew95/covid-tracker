9805f42afb84a57523e5385048f4180c0e1bf4bb
Clew95 <marco.quintano1995@gmail.com>
2020-09-08 17:01:20 +0200

    update

--- routes/covid.js
+++ routes/covid.js
@@ -11,22 +11,28 @@ router.get('/', (req, res) => {
 			const DATA_BUNCH = response.data;
 			const TODAY = DATA_BUNCH.length - 1;
 			const DATA_SETTINGS = {
-				incremento_nuovi_positivi:
+				incremento_nuovi_positivi       :
 					parseInt(DATA_BUNCH[TODAY]['nuovi_positivi']) - parseInt(DATA_BUNCH[TODAY - 1]['nuovi_positivi']) >
 					0
 						? true
 						: false,
-				incremento_attualmente_positivi:
+				incremento_attualmente_positivi :
 					parseInt(DATA_BUNCH[TODAY]['variazione_totale_positivi']) > 0 ? true : false,
-				incremento_terapia_intensiva:
+				incremento_terapia_intensiva    :
 					parseInt(DATA_BUNCH[TODAY]['terapia_intensiva']) -
 						parseInt(DATA_BUNCH[TODAY - 1]['terapia_intensiva']) >
 					0
 						? true
-						: false
+						: false,
+				isARegion                       : false
 			};
 
-			res.render('covid', { baseUrl: req.baseUrl, data: DATA_BUNCH, settings: DATA_SETTINGS });
+			res.render('covid', {
+				baseUrl   : req.baseUrl,
+				data      : DATA_BUNCH,
+				settings  : DATA_SETTINGS,
+				isARegion : DATA_SETTINGS.isARegion
+			});
 		});
 });
 
--- routes/regioni.js
+++ routes/regioni.js
@@ -7,21 +7,45 @@ router.get('/', (req, res) => {
 		.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json')
 		.then((response) => {
 			const DATA_BUNCH = response.data;
-			res.render('regioni', { baseUrl: req.baseUrl, data: DATA_BUNCH });
+			res.render('regioni', { baseUrl: req.baseUrl, data: DATA_BUNCH, isARegion: false });
 		});
 });
 
 router.get('/:regione', (req, res) => {
-	res.render('working', { baseUrl: req.baseUrl });
+	//res.render('working', { baseUrl: req.baseUrl });
 
-	/*	axios
+	axios
 		.get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json')
 		.then((response) => {
 			const DATA_BUNCH = response.data.filter(function(e) {
 				return e.denominazione_regione.toLowerCase() === req.params.regione;
 			});
 
-			res.render('regionDetails', { baseUrl: req.baseUrl, data: DATA_BUNCH });
-		});*/
+			const TODAY = DATA_BUNCH.length - 1;
+
+			const DATA_SETTINGS = {
+				incremento_nuovi_positivi       :
+					parseInt(DATA_BUNCH[TODAY]['nuovi_positivi']) - parseInt(DATA_BUNCH[TODAY - 1]['nuovi_positivi']) >
+					0
+						? true
+						: false,
+				incremento_attualmente_positivi :
+					parseInt(DATA_BUNCH[TODAY]['variazione_totale_positivi']) > 0 ? true : false,
+				incremento_terapia_intensiva    :
+					parseInt(DATA_BUNCH[TODAY]['terapia_intensiva']) -
+						parseInt(DATA_BUNCH[TODAY - 1]['terapia_intensiva']) >
+					0
+						? true
+						: false,
+				isARegion                       : true
+			};
+
+			res.render('regionDetails', {
+				baseUrl   : req.baseUrl,
+				data      : DATA_BUNCH,
+				settings  : DATA_SETTINGS,
+				isARegion : DATA_SETTINGS.isARegion
+			});
+		});
 });
 module.exports = router;
